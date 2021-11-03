40fba7875929ec78faefe83da9c2cb76a91c5199
Oskar Helling <oskar.helling@gmail.com>
2021-11-03 13:22:47 +0100

    Comments to help to find the right place to comment out in order to fix the problem

--- src/app/feature-a/feature-a-routing.module.ts
+++ src/app/feature-a/feature-a-routing.module.ts
@@ -5,7 +5,7 @@ import { FeatureAPage as FeatureAPage } from './feature-a.page';
 
 const routes: Routes = [
   { path: '',
-    component: FeatureAShellComponent,
+    component: FeatureAShellComponent, // Comment out this line to fix the back-button. Do the same in Feature B
     children: [
       { path: '', component: FeatureAPage },
       { path: 'list-page', loadChildren: () => import('./list/list.module').then( m => m.ListPagePageModule) }
--- src/app/feature-b/feature-b-routing.module.ts
+++ src/app/feature-b/feature-b-routing.module.ts
@@ -6,7 +6,7 @@ import { FeatureBShellComponent } from './feature-b-shell.component';
 
 const routes: Routes = [
   { path: '',
-    component: FeatureBShellComponent,
+    component: FeatureBShellComponent, // Comment out this line to fix the back-button. Do the same in Feature A
     children: [
       { path: '', component: FeatureBPage }
     ]}
